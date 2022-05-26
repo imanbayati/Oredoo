42c97ffa9ec81d181a3170b685ff3f81790a0ee3
imanbayati <bayati.pro@gmail.com>
2022-05-26 17:21:32 +0430

    first model
    created model of posts for blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_date', 'published_date','status')
+    fields = ( 'title',)
+    empty_value_display = '-empty-'
+    date_hierarchy = 'published_date'
+    list_filter = ('title', 'created_date', 'published_date','status')
+    search_fields = ('title', 'created_date', 'published_date','status')
+
+admin.site.register(Post,PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(max_length=255)
+    #category
+    #tags
+    img = models.ImageField()
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+    status = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title  
