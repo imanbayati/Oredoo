2e980d55d0cc0449a4d4e6a4899484c3717d38ea
imanbayati <bayati.pro@gmail.com>
2022-06-02 00:35:10 +0430

    messages
    use bootstrap alert messages for newsletter form to show email submited was successfully or not

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Post,Category
 from website.forms import NewsletterForm
+from django.contrib import messages
 
 
 # Create your views here.
@@ -12,6 +13,9 @@ def home_page(request):
         form = NewsletterForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request,'email submited')
+        else:
+            messages.error(request,'email was not submited')
     form = NewsletterForm() 
     context = {'categoies': categoies,"form":form}
     return render(request,'website/home.html',context)
