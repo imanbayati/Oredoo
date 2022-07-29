66230e32ecf982f27e5f46bf22e65a1bcf699b25
imanbayati <bayati.pro@gmail.com>
2022-07-29 19:34:01 +0430

    show onlne

--- oredoo/settings.py
+++ oredoo/settings.py
@@ -151,9 +151,9 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'statics',
-]
+#STATICFILES_DIRS = [
+    #BASE_DIR / 'statics',
+#]
 
 #STATIC_ROOT = BASE_DIR / 'static'
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
