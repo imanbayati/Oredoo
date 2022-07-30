d6c1258889b9d83014daf8c2bc3335575b58c0a0
imanbayati <99612400+imanbayati@users.noreply.github.com>
2022-07-30 13:02:37 +0430

    show online

--- oredoo/settings.py
+++ oredoo/settings.py
@@ -102,10 +102,10 @@ WSGI_APPLICATION = 'oredoo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR /'d4e5ffdkm00vtu',
-        'USER':'gjeybpnpuhwhkj',
-        'PASSWORD':'d7f4853b1316e2300540e25e521bade977f6e2eac1a6dbae0c243750182fad45',
-        'HOST':'ec2-44-206-214-233.compute-1.amazonaws.com',
+        'NAME': BASE_DIR /'dd5fu9ie13b82j',
+        'USER':'tnilwzjhxydsfl',
+        'PASSWORD':'b1ba8df43e69bb0cac9607c683e4683dd3692b92197e7e3792feaf3613f17353',
+        'HOST':'ec2-44-206-197-71.compute-1.amazonaws.com',
         'PORT':'5432'
         }
 }
